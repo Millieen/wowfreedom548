239f21782baa94bd08cff633829ff9a522055ce4
Azeroc <azeroc2601@gmail.com>
2015-03-02 22:21:28 +0200

    Added: script to set everyone's spawn-point to the mall place.

---
+++ sql/freedom/update_world_playercreateinfo.sql
@@ -0,0 +1,10 @@
+SET @MAP := 630; SET @ZONE := 4384; SET @POS_X := 836.952; SET @POS_Y = -108.702; SET @POS_Z = 111.747; SET @POS_O = 0.089445;
+START TRANSACTION;
+UPDATE world.playercreateinfo SET
+	map = @MAP;
+	zone = @ZONE;
+	position_x = @POS_X;
+	position_y = @POS_Y;
+	position_z = @POS_Z;
+	orientation = @POS_O;							  
+COMMIT;
