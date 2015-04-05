556fddd3aff504c3f261aa38530e10ff3e6c21ca
Azeroc <azeroc2601@gmail.com>
2015-04-05 16:10:55 +0300

    Removed freedom SQL scripts, they are moved to binary repo.

--- sql/freedom/alter_world_creature_1_addcolumns.sql
+++
@@ -1,6 +0,0 @@
-USE world;
-ALTER TABLE `creature`ADD COLUMN `size` FLOAT NOT NULL DEFAULT '1' AFTER `dynamicflags`;
-UPDATE IGNORE creature SET size = (SELECT size FROM creature_template WHERE entry = creature.id);
-ALTER TABLE `creature` ADD COLUMN `owner` VARCHAR(32) NOT NULL DEFAULT '' AFTER `size`;
-ALTER TABLE `creature` ADD COLUMN `editor` VARCHAR(32) NOT NULL DEFAULT '' AFTER `owner`;
-ALTER TABLE `creature` ADD COLUMN `created` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP AFTER `editor`;
