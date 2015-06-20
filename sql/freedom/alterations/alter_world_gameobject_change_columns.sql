a2b302bee23d1a8fe9a6faa958dc3c8a57f64552
Azeroc <azeroc2601@gmail.com>
2015-06-20 18:49:15 +0300

    Changed column `owner` to `creator` for better consistency.

--- sql/freedom/alterations/alter_world_gameobject_change_columns.sql
+++ sql/freedom/alterations/alter_world_gameobject_change_columns.sql
@@ -1,7 +1,7 @@
 USE `world`;
 ALTER TABLE `gameobject`
 	ADD COLUMN `size` FLOAT NOT NULL DEFAULT '1' COMMENT 'In-game scale of the object' AFTER `state`,
-	ADD COLUMN `owner` INT(10) UNSIGNED NULL DEFAULT '0' COMMENT 'Creator of the game object' AFTER `size`,
+	ADD COLUMN `creator` INT(10) UNSIGNED NULL DEFAULT '0' COMMENT 'Creator of the game object' AFTER `size`,
 	ADD COLUMN `editor` INT(10) UNSIGNED NULL DEFAULT '0' COMMENT 'Last account what modified the object' AFTER `owner`,
 	ADD COLUMN `created` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'When object was first created/spawned' AFTER `editor`,
 	ADD COLUMN `modified` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'When object was last modified (moved, turned, scaled)' AFTER `created`;
