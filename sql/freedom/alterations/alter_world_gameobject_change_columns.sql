496f711616814cf3a7e793910c35ade1a2cd05cf
Kretol <kretol@conquestofthehorde.com>
2015-07-17 20:12:11 -0700

    More SQL fixes!

--- sql/freedom/alterations/alter_world_gameobject_change_columns.sql
+++ sql/freedom/alterations/alter_world_gameobject_change_columns.sql
@@ -2,8 +2,8 @@ USE `world`;
 ALTER TABLE `gameobject`
 	ADD COLUMN `size` FLOAT NOT NULL DEFAULT '1' COMMENT 'In-game scale of the object' AFTER `state`,
 	ADD COLUMN `creator` INT(10) UNSIGNED NULL DEFAULT '0' COMMENT 'Creator of the game object' AFTER `size`,
-	ADD COLUMN `editor` INT(10) UNSIGNED NULL DEFAULT '0' COMMENT 'Last account what modified the object' AFTER `owner`,
+	ADD COLUMN `editor` INT(10) UNSIGNED NULL DEFAULT '0' COMMENT 'Last account what modified the object' AFTER `creator`,
 	ADD COLUMN `created` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'When object was first created/spawned' AFTER `editor`,
 	ADD COLUMN `modified` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'When object was last modified (moved, turned, scaled)' AFTER `created`;
 	
-UPDATE gameobject SET size = (SELECT size FROM gameobject_template WHERE entry = gameobject.id);
+UPDATE IGNORE gameobject SET size = (SELECT size FROM gameobject_template WHERE entry = gameobject.id);
